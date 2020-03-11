62fdb451eea2067613574ca91e177c3a237850d5
OdearOgy <hovhan198@gmail.com>
2020-03-11 05:33:24 +0400

    Removed redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import store from './store';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<Provider store={store}>
-		<Router>
-			<App />
-		</Router>
-	</Provider>
+	<Router>
+		<App />
+	</Router>
 );
