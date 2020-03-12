8d03f8df36039ba675cba3e1f9c18b6d6c830993
OdearOgy <hovhan198@gmail.com>
2020-03-12 11:24:57 +0400

    Fixed cookies error

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<CookiesProvider>
-		<Router>
-			<App />
-		</Router>
-	</CookiesProvider>
+	<Router>
+		<App />
+	</Router>
 );
